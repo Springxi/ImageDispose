616df836f6963ff74c2bfd3d026434897f254a0a
Lilly <752488291@qq.com>
2019-07-06 17:10:22 +0800

    Update Semi-RLNet.py

--- ImageProject/Semi-RLNet.py
+++ ImageProject/Semi-RLNet.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 26 14:14:00 2017
 
-@author: ubuntu-507
-"""
 
 from __future__ import absolute_import
 from __future__ import division
